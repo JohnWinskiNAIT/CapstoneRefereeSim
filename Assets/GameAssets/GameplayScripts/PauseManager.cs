d3b9dad12e9cf9d53ad54d4fd40d9e1a0dcba6e1
Aidan <ahoffschild@outlook.com>
2024-01-25 14:30:35 -0700

    controls update

---
+++ Assets/GameAssets/GameplayScripts/PauseManager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseManager : MonoBehaviour
+{
+    public static PauseManager instance;
+
+    //Reference UI parent this needs to enable and disable along with individual parts it needs to control.
+
+
+    // Declares itself instance. Do *NOT* give this DontDestroyOnLoad.
+    // This should be destroyed and recreated when swapping from main menu to gameplay.
+    // Game data and settings should be stored in common scripts.
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = new PauseManager();
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void PauseGame()
+    {
+
+    }
+}
--- Assets/GameAssets/GameplayScripts/PlayerControl.cs
+++ Assets/GameAssets/GameplayScripts/PlayerControl.cs
@@ -8,9 +8,14 @@ public class PlayerControl : MonoBehaviour
     [SerializeField]
     InputActionAsset inputActions;
     private Rigidbody rb;
+    private Camera mainCamera;
+
+    [SerializeField]
+    float maxSpeed, accelerationSpeed, breakingSpeed;
 
     private InputAction moveAction, lookAction, callAction, whistleAction, pauseAction;
     private Vector2 moveInput, lookInput;
+    private Vector3 cameraAngle;
 
     // Makes sure to get all actions on Awake as opposed to start, otherwise OnEnable goes first.
     void Awake()
@@ -22,18 +27,49 @@ public class PlayerControl : MonoBehaviour
         pauseAction = inputActions.FindActionMap("Gameplay").FindAction("Pause");
     }
 
+    private void Start()
+    {
+        cameraAngle = mainCamera.transform.rotation.eulerAngles;
+    }
+
     // Update is called once per frame
     void Update()
     {
         // Getting Vector2 inputs for move and look.
         moveInput = moveAction.ReadValue<Vector2>();
         lookInput = lookAction.ReadValue<Vector2>();
+
+        //Call pausemanager when pause is pressed.
+        if (pauseAction.WasPressedThisFrame())
+        {
+            PauseManager.instance.PauseGame();
+        }
     }
 
     private void FixedUpdate()
     {
-        
+        //Do all movement-related changes in here.
+        PlayerMovement();
+
+        //Camera logic might need to be determined here then visuals performed in late update.
     }
 
+    private void PlayerMovement()
+    {
+        //Apply force.
+        rb.AddRelativeForce(moveInput * accelerationSpeed * Time.fixedDeltaTime, ForceMode.Force);
 
+        //Apply cap if greater than max speed. (Parabolic acceleration curve for later?)
+        Vector2 capTest = new Vector2(rb.velocity.x, rb.velocity.z);
+        if (capTest.magnitude > maxSpeed)
+        {
+            rb.velocity = new Vector3(rb.velocity.normalized.x * maxSpeed, rb.velocity.y, rb.velocity.normalized.z);
+        }
+    }
+
+    private void LateUpdate()
+    {
+        //Do all camera-related changes in here.
+
+    }
 }
