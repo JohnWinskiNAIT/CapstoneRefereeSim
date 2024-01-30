65bebf69a5e9813b4845420f7dc9c806868aaf8e
Benjamin Wlasichuk <bwlasichuk1@nait.ca>
2024-01-30 14:19:43 -0700

    GoalieScript foundation added.

---
+++ Assets/GameAssets/GameplayScripts/GoalieScript.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoalieScript : MonoBehaviour
+{
+    [SerializeField] bool letScore;
+    player goaliePlayer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        goaliePlayer = new player();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (goaliePlayer.puckPossesion)
+        {
+            if (letScore)
+            {
+                //insert goal manager score thing here.
+            }
+            else
+            {
+                //insert save mechanic here.
+            }
+        }
+    }
+
+}
