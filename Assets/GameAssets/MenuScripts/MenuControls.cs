354842a398b85286452e27dd87cbe4997a17e450
KsmLeopard09 <mangatkuljyot95@gmail.com>
2024-02-06 13:58:24 -0700

    MenuHoverSetup

--- Assets/GameAssets/MenuScripts/MenuControls.cs
+++ Assets/GameAssets/MenuScripts/MenuControls.cs
@@ -1,18 +1,31 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using TMPro;
+using UnityEngine.InputSystem;
+using UnityEngine.XR;
 
 public class MenuControls : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] Button[] buttons;
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        for(int i  = 0; i < buttons.Length; i++)
+        {
+            buttons[i].GetComponentInChildren<TextMeshProUGUI>().fontMaterial.SetFloat(ShaderUtilities.ID_GlowPower, 0);
+        }
+    }
+    public void Glow(GameObject callingObject)
+    {
+        callingObject.GetComponentInChildren<TextMeshProUGUI>().fontMaterial.SetFloat(ShaderUtilities.ID_GlowPower, 0.3f);
+    }
+    public void NoGlow(GameObject callingObject)
+    {
+        callingObject.GetComponentInChildren<TextMeshProUGUI>().fontMaterial.SetFloat(ShaderUtilities.ID_GlowPower, 0);
+    }
+    public void Options()
+    {
+
     }
 }
