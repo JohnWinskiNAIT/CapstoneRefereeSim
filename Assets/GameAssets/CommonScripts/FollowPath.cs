fac8adf99c03715cd6d1d82a9333d27e474b951e
Benjamin Wlasichuk <bwlasichuk1@nait.ca>
2024-02-08 14:48:35 -0700

    Added Comments.

--- Assets/GameAssets/CommonScripts/FollowPath.cs
+++ Assets/GameAssets/CommonScripts/FollowPath.cs
@@ -1,10 +1,12 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class FollowPath : MonoBehaviour
 {
-    public Vector3[] path = new Vector3[0];
+    //This is a script defining the path, and generically moves an object forward on the path.
+    public PathWaypoint[] path = new PathWaypoint[0];
     // Start is called before the first frame update
     void Start()
     {
@@ -12,16 +14,25 @@ public class FollowPath : MonoBehaviour
     }
 
     // Update is called once per frame
-    private IEnumerator StartFollowingPath(Vector3[] pathToFollow)
+    private IEnumerator StartFollowingPath(PathWaypoint[] pathToFollow)
     {
         foreach (var point in  pathToFollow)
         {
-            while (Vector3.Distance(a: transform.position, b: point) > 0.1f)
+            while (Vector3.Distance(a: transform.position, b: point.waypointPosition) > 0.1f)
             {
-                transform.position = Vector3.MoveTowards(current: transform.position, target: point, maxDistanceDelta: Time.deltaTime * 1);
+                transform.position = Vector3.MoveTowards(current: transform.position, target: point.waypointPosition, maxDistanceDelta: Time.deltaTime * 1);
 
                 yield return new WaitForEndOfFrame();
             }
         }
     }
+}
+
+[Serializable]
+public class PathWaypoint
+{
+    public Vector3 waypointPosition;
+    public float timeAtWaypoint;
+
+
 }
--- Assets/GameAssets/Editor/FollowPathEditor.cs
+++ Assets/GameAssets/Editor/FollowPathEditor.cs
@@ -3,34 +3,42 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 using UnityEditor.SceneManagement;
+using Unity.VisualScripting;
 
 [CustomEditor(typeof(FollowPath))]
+
+//Script is designed to make features available in the editor so we can easily make paths for our hockey players to follow.
 public class FollowPathEditor : Editor
 {
+    //script follows the path 
     private FollowPath targetComponent;
 
     private void OnSceneGUI()
     {
         targetComponent = (FollowPath)target;
 
-        Handles.color = Color.cyan;
+        Handles.color = Color.red;
 
         var positions = targetComponent.path;
 
+        //for each item in the list, draw a dotted line between them.
         for (int i = 1; i < positions.Length + 1; ++i)
         {
-            var previousPoint = positions[i - 1];
-            var currentPoint = positions[i % positions.Length];
-
+            //first point
+            var previousPoint = positions[i - 1].waypointPosition;
+            //second point
+            var currentPoint = positions[i % positions.Length].waypointPosition;
+            //drawing the line between the points
             Handles.DrawDottedLine(p1: previousPoint, p2: currentPoint, screenSpaceSize: 4f);
         }
 
-
+        //create position handles on the points so you can move them
         for (int i = 0; i < positions.Length; ++i)
         {
-            positions[i] = Handles.PositionHandle(positions[i], Quaternion.identity);
+            
+            positions[i].waypointPosition = Handles.PositionHandle(positions[i].waypointPosition, Quaternion.identity);
         }
-
+        //when you move a point, make unity saveable
         if (GUI.changed)
         {
             EditorUtility.SetDirty(targetComponent);
