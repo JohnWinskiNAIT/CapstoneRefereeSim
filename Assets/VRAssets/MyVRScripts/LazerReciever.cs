866d4e5625e77c743df2f9519cc0353069535163
EvanHolmes <squippysquerp@gmail.com>
2024-02-25 16:26:24 -0700

    Updated Lazer scripts

--- Assets/VRAssets/MyVRScripts/LazerReciever.cs
+++ Assets/VRAssets/MyVRScripts/LazerReciever.cs
@@ -1,18 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI ;
 
 public class LazerReciever : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    bool isPointedAt = false;    //is the raycast looking at it
+    bool isActivated;
+    Button button;
+
+    private void Update()
     {
-        
+        PointedAtUpdate();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void PointedAtUpdate()
     {
-        
+        if (isPointedAt)
+        {
+            Debug.Log($"Is being pointed at");
+        }
+    }
+
+    public void Activate()
+    {
+        Debug.Log($"Is being Activated");
+
+        //button.clickable.clicked += true;
+
+        //m_OnClick.m_PersistentCalls.m_Calls;
+
+        //m_OnClick.m_PersistentCalls.m_Calls.Array.data[0].m_Target
+
+        //button.onClick.GetPersistentMethodName(0);
+
+        Debug.Log($"{button.onClick.GetPersistentMethodName(0)}"); 
     }
 }
