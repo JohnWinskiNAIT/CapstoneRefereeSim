2a3b0729d395063495f64747f68d11e92c56ec17
EvanHolmes <squippysquerp@gmail.com>
2024-03-03 21:15:41 -0700

    Finished up incomplete code. 0.5 hours

--- Assets/VRAssets/MyVRScripts/LazerReciever.cs
+++ Assets/VRAssets/MyVRScripts/LazerReciever.cs
@@ -8,16 +8,25 @@ public class LazerReciever : MonoBehaviour
     bool isPointedAt = false;    //is the raycast looking at it
     bool isActivated;
     Button button;
+    Collider coll;
     Vector3 hitPos = Vector3.zero;
     Vector3 center;
-    Vector3 distFromCenter;
+    Vector2 center2D;
+    
     Vector2 distFromCenterSimplified;
+    Vector2 hitPos2D;
 
-    GameObject cursorRepresentative;
+    [SerializeField] GameObject cursorParent;
+    [SerializeField] GameObject bottLeftBound;  //empty that wont move, just there to tell us where the bounds are in 2D
+    Vector3 bottLeftPos;    //just to shorten the code writing.
+    Vector2 bottLeftPos2D;  //just in case there is any weird z axis shenanigans. 
 
     private void Start()
     {
+        coll = GetComponent<Collider>();
         center = transform.position;
+        bottLeftPos = bottLeftBound.transform.localPosition;
+        bottLeftPos2D = new Vector2(bottLeftPos.x, bottLeftPos.y);
         button = GetComponentInParent<Button>();
     }
 
@@ -38,16 +47,26 @@ public class LazerReciever : MonoBehaviour
 
     public Vector3 DistFromCenter()
     {
-        distFromCenter = hitPos - center;
+        if (hitPos == Vector3.zero)
+        {
+            Debug.Log("HitPosition = Zero. Reciever has not been pointed at ever.");
+        }
+        Vector3 distFromCenter = hitPos - center;
         return distFromCenter;
     }
 
-    public Vector2 DistFromCenter2D()
-    {
-        cursorRepresentative.transform.position = hitPos;
-        //distFromCenterSimplified = new Vector2(distFromCenter.);
-        return distFromCenterSimplified;
+    public Vector2 CursorPos2D()
+    {   //returns the losition of the pointer as if the canvas was a 2d flat screen.
+        cursorParent.transform.position = hitPos;
+        return cursorParent.transform.localPosition;
     }
+
+    public Vector2 CursorPosScreen()
+    {   //Returns cursor position as if it was measuring from Bottom Left of the screen
+        return CursorPos2D() - bottLeftPos2D;
+    }
+
+
 
     public void Activate()
     {
