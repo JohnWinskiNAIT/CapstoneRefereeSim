c7943d98333e8d652241f7e1e7ba597fe02b4260
Aidan <ahoffschild@outlook.com>
2024-03-04 23:20:27 -0700

    Working solution to the knot issue

--- Assets/GameAssets/TestItems/SplineGeneratorTest.cs
+++ Assets/GameAssets/TestItems/SplineGeneratorTest.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using Unity.Mathematics;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Splines;
 using UnityEngine.UIElements;
@@ -16,6 +17,9 @@ public class SplineGeneratorTest : MonoBehaviour
     float minimumMagnitude, maximumMagnitude;
     
     float splineAreaMaxX, splineAreaMinX, splineAreaMaxZ, splineAreaMinZ;
+
+    [SerializeField]
+    GameObject testingTheThing;
 
     // Start is called before the first frame update
     void Start()
@@ -51,9 +55,30 @@ public class SplineGeneratorTest : MonoBehaviour
     {
         BezierKnot[] splineKnots = new BezierKnot[pointCount];
 
-        for (int i = 0; i < pointCount; i++)
+        Vector3 nextPosition = Vector3.zero;
+
+        //this breaks the ability to compile? I don't get it
+        nextPosition = new Vector3(Random.Range(splineAreaMinX, splineAreaMaxX), 0, Random.Range(splineAreaMinZ, splineAreaMaxZ));
+        splineKnots[0] = new BezierKnot();
+        splineKnots[0].Position = nextPosition;
+        splineKnots[0].Rotation = Quaternion.identity;
+
+        for (int i = 1; i < pointCount; i++)
         {
-            Vector3 nextPosition = Vector3.zero;
+            Vector3 lastPosition = nextPosition;
+
+            while ((nextPosition - lastPosition).magnitude < minimumMagnitude)
+            {
+                nextPosition = new Vector3(Random.Range(splineAreaMinX, splineAreaMaxX), 0, Random.Range(splineAreaMinZ, splineAreaMaxZ));
+            }
+
+            splineKnots[i] = new BezierKnot();
+            splineKnots[i].Position = nextPosition;
+            splineKnots[i].Rotation = Quaternion.identity;
+        }
+
+        /*for (int i = 0; i < pointCount; i++)
+        {
             while (nextPosition.magnitude < minimumMagnitude)
             {
                 nextPosition = new Vector3(Random.Range(splineAreaMinX, splineAreaMaxX), 0, Random.Range(splineAreaMinZ, splineAreaMaxZ));
@@ -62,8 +87,7 @@ public class SplineGeneratorTest : MonoBehaviour
             splineKnots[i] = new BezierKnot();
             splineKnots[i].Position = nextPosition;
             splineKnots[i].Rotation = Quaternion.identity;
-            //
-        }
+        }*/
 
         spline.Clear();
 
@@ -79,6 +103,8 @@ public class SplineGeneratorTest : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //AI will rotate on the Z axis and make unpredictable and weird motions during very sharp or irregular knots.
+        //This does a good job of fixing the issue for now until we can identify a potentially more satisfying solution.
+        testingTheThing.transform.localRotation = new Quaternion(0, testingTheThing.transform.rotation.y, 0, testingTheThing.transform.rotation.w);
     }
 }
