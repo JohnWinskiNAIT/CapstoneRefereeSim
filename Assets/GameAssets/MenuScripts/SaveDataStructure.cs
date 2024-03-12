6ff00324e2aae4a6aba607f568edcf7ef3bc46c3
KsmLeopard09 <mangatkuljyot95@gmail.com>
2024-03-11 23:22:06 -0600

    Changed audio setting saving from playerPrefs to xml file

---
+++ Assets/GameAssets/MenuScripts/SaveDataStructure.cs
@@ -0,0 +1,10 @@
+using System;
+using UnityEngine;
+
+[Serializable]
+public struct VolumeData
+{
+    public float masterVolume;
+    public float SFXvolume;
+    public float ambientVolume;
+}
--- Assets/GameAssets/MenuScripts/Settings.cs
+++ Assets/GameAssets/MenuScripts/Settings.cs
@@ -3,48 +3,58 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.Audio;
+using System.IO;
+using System.Xml.Serialization;
+
 public class Settings : MonoBehaviour
 {
     [SerializeField] Slider masterSlider;
     [SerializeField] Slider SFXSlider;
     [SerializeField] Slider ambientSlider;
     [SerializeField] AudioMixer audioMixer;
-    public float test;
+    public VolumeData myVolumes;
+    string filePath;
+    string rootPath = "SaveData\\";
     private void Start()
     {
-        if(PlayerPrefs.HasKey("MasterVolume"))
-        {
-            masterSlider.value = PlayerPrefs.GetFloat("MasterVolume");
-        }
-        if (PlayerPrefs.HasKey("SFXVolume"))
-        {
-            SFXSlider.value = PlayerPrefs.GetFloat("SFXVolume");
-        }
-        if (PlayerPrefs.HasKey("AmbientVolume"))
-        {
-            ambientSlider.value = PlayerPrefs.GetFloat("AmbientVolume");
-        }
+        myVolumes = new VolumeData();
+        filePath = rootPath + "volumeData\\volumes.dat";
+        LoadVolumes();
+        masterSlider.value = myVolumes.masterVolume;
+        SFXSlider.value = myVolumes.SFXvolume;
+        ambientSlider.value = myVolumes.ambientVolume;
     }
     public void ChangeMasterVolume()
     {
         float volume = masterSlider.value;
         audioMixer.SetFloat("Master", Mathf.Log10(volume) * 20);
-        PlayerPrefs.SetFloat("MasterVolume", volume);
-        PlayerPrefs.Save();
+        SaveVolume();
     }
     public void ChangeSFXVolume()
     {
         float volume = SFXSlider.value;
         audioMixer.SetFloat("SFX", Mathf.Log10(volume) * 20);
-        PlayerPrefs.SetFloat("SFXVolume", volume);
-        PlayerPrefs.Save();
+        SaveVolume();
     }
     public void ChangeAmbientVolume()
     {
         float volume = ambientSlider.value;
         audioMixer.SetFloat("Ambient", Mathf.Log10(volume) * 20);
-        PlayerPrefs.SetFloat("AmbientVolume", volume);
-        PlayerPrefs.Save();
+        SaveVolume();
     }
-
+    public void SaveVolume()
+    {
+        myVolumes.masterVolume = masterSlider.value;
+        myVolumes.SFXvolume = SFXSlider.value;
+        myVolumes.ambientVolume = ambientSlider.value;
+        if (!Directory.Exists("SaveData\\volumeData"))
+        {
+            Directory.CreateDirectory("SaveData\\volumeData");
+        }
+        SaveManager.SaveData(filePath, ref myVolumes);
+    }
+    public void LoadVolumes()
+    {
+        SaveManager.LoadData(filePath, ref myVolumes);
+    }
 }
