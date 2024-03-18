7b58af4685b02fa5ab674c28a9a33819f4d67641
KsmLeopard09 <mangatkuljyot95@gmail.com>
2024-03-18 14:19:55 -0600

    Evan pull this please

--- Assets/GameAssets/MenuScripts/Settings.cs
+++ Assets/GameAssets/MenuScripts/Settings.cs
@@ -90,6 +90,9 @@ public class Settings : MonoBehaviour
         }
         else
         {
+            masterSlider.value = 1;
+            SFXSlider.value = 1;
+            ambientSlider.value = 1;
             for (int i = 0; i < mySettings.penalties.Length; i++)
             {
                 mySettings.penalties[i].isEnabled = myPenaltyToggles[i].isOn;
@@ -113,6 +116,8 @@ public class Settings : MonoBehaviour
             if (masterSlider.value > 0.0001f || SFXSlider.value > 0.0001f || ambientSlider.value > 0.0001f)
             {
                 mySettings.lastMasterVolume = masterSlider.value;
+                mySettings.lastSFXvolume = SFXSlider.value;
+                mySettings.lastAmbientVolume = ambientSlider.value;
             }
             masterSlider.value = 0.0001f;
             ChangeMasterVolume();
