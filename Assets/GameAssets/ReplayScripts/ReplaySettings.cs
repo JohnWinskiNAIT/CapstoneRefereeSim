0c438ba1f60c073f9fefc6fbc96f5b86ff155652
Aidan <ahoffschild@outlook.com>
2024-04-01 01:47:28 -0600

    setting up replay scene

---
+++ Assets/GameAssets/ReplayScripts/ReplaySettings.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class ReplaySettings
+{
+    public const string FILEPATH = "SaveData\\PositionData";
+    public const float Tick_Rate = 0.25f;
+}
